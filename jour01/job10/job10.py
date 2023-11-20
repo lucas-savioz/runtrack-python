d1451b5b861d0c7737e658a4fe4d0034247b6ac7
lucas <lucas.savioz@laplateforme.io>
2023-11-20 17:31:16 +0100

    Rendu du projet runtrack-python - jour1

---
+++ jour01/job10/job10.py
@@ -0,0 +1,29 @@
+# ETAPE 1 
+
+initial_invest = 5000
+taux_rendement_annuel = 0.02
+
+# Calcul rendement total annuel
+rendement_annuel = initial_invest * taux_rendement_annuel
+
+print("Investissment initial :", initial_invest)
+print("Taux de rendement annuel :", taux_rendement_annuel)
+
+# Gain total après un année
+print("Capitable après investissement : ")
+print(initial_invest + rendement_annuel)
+
+# Etape 2 
+
+initial_invest_2 = 4590
+taux_rendement_annuel_2 = 0.01
+
+# Calcul rendement total annuel
+rendement_annuel_2 = initial_invest_2 * taux_rendement_annuel_2
+
+print("Investissment initial :", initial_invest_2)
+print("Taux de rendement annuel (deuxième année) :", taux_rendement_annuel_2)
+
+# Gain total après un année
+print("Capitable après investissement (deuxième année) : ")
+print(initial_invest_2 + rendement_annuel_2)
