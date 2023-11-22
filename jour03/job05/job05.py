05b722eedbc6a6d7c00294b96fb5534e49250ef2
lucas <lucas.savioz@laplateforme.io>
2023-11-22 16:29:32 +0100

    jour03 job05 fini

---
+++ jour03/job05/job05.py
@@ -0,0 +1,29 @@
+# Définir la fonction est ses paramètres
+def calcule(num1, operator, num2):
+    # Conditions
+    # Si operator est strictement égale à '+'
+    if operator == '+':
+        # alors result égale à num1 + num2
+        result = num1 + num2
+        # Sinon si operator est strictement égale à '-'
+    elif operator == '-':
+        # alors result égale à num1 + num2
+        result = num1 - num2
+    elif operator == '*':
+        result = num1 * num2
+    elif operator == '/':
+        result = num1 / num2
+    elif operator == '%':
+        result = num1 % num2
+    
+    # Imprime le contenu de la f string qui formate l'ensemble des variables
+    print(f"{num1} {operator} {num2} = {result}")
+
+# Affiche l'ensemble des calculs de la fonction calcule et de ses paramètres
+calcule(2, '+', 5)
+calcule(2, '-', 5)
+calcule(2, '*', 5)
+calcule(2, '/', 5)
+calcule(2, '%', 5)
+
+
