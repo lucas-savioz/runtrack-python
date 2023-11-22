379c4c9999eecb5a6ff855050d08190c24d9c746
lucas <lucas.savioz@laplateforme.io>
2023-11-22 13:52:15 +0100

    jour03 job03 fini

---
+++ jour03/job02/job02.py
@@ -0,0 +1,14 @@
+# Ajout de différents paramètres
+name = ["Lucas", "Pierre", "Alicia"]
+
+def My_print_name(name):
+    print(name)
+
+# Appel la fonction avec les différents paramètres
+My_print_name("Lucas")
+My_print_name("Pierre")
+My_print_name("Alicia")
+
+# OU
+# for person in name:
+#     My_print_name(person)
