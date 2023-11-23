6728f16ff6260df62ed326110aabb3d84b2f23e7
lucas <lucas.savioz@laplateforme.io>
2023-11-23 16:06:01 +0100

    jour04 job02 fini

---
+++ jour04/job02/job02.py
@@ -0,0 +1,8 @@
+def fruits(liste):
+    
+    # Selectionne le deuxième éléments partant de 0 donc 1 sera "cerise".
+    print(liste[1])
+
+ma_liste = ["pomme", "cerise", "orange"]
+
+fruits(ma_liste)
