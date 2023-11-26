9505dce36bd35c97e09a6b72235da7dbaa887440
lucas <lucas.savioz@laplateforme.io>
2023-11-26 18:20:56 +0100

    jour04 job13 fini

--- jour04/job12/job12.py
+++ jour04/job12/job12.py
@@ -3,3 +3,22 @@ print(L)
 
 L[0], L[1], L[2], L[3], L[4], L[5], L[6] = L[0], L[2], L[1], L[5], L[4], L[6], L[3]
 print(L)
+
+
+
+# Sinon ça
+
+# L = [1, 5, 3, 80, 25, 18, 40]
+# print(L)
+
+# n = 0
+# # "_" c'est les éléments de la liste
+# for _ in L: 
+#     n += 1
+
+# for i in range(n - 1):
+#     for j in range(0, n - i - 1):
+#         if L[j] > L[j + 1]:
+#             L[j], L[j + 1] = L[j + 1], L[j]
+
+# print(L)
---
+++ jour04/job13/job13.py
@@ -0,0 +1,6 @@
+L = [10, 20, 30, 20, 10, 50, 60, 40, 80, 50, 40]
+print(L)
+
+# On utilise del, ça supprime en direct chaque élément de la liste et repart à zéro
+del L[3], L[3], L[7], L[7]
+print(L)
